e9c09f0c6ba6ced358b0d4b4f0c09042e45ea214
AbyssaI <31418116+AbyssaI@users.noreply.github.com>
2017-08-29 21:11:47 -0700

    10.1.1

--- audio-router-gui/window.cpp
+++ audio-router-gui/window.cpp
@@ -2,7 +2,7 @@
 
 telemetry* telemetry_m = NULL;
 
-window::window(/*bootstrapper* bootstrap*/) : dlg_main_b(true)/*, license(NULL)*//*, bootstrap(bootstrap)*/
+window::window(bootstrapper* bootstrap) : dlg_main_b(true)/*, license(NULL)*/, bootstrap(bootstrap)
 {
     this->dlg_main = new dialog_main(*this);
     this->form_view = new formview(*this);
@@ -78,8 +78,8 @@ LRESULT window::OnFileRefreshlist(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWn
 LRESULT window::OnAbout(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
 {
     this->MessageBoxW(
-        L"Audio Router version 0.10.2.\n" \
-        L"\nIf you come across any bugs(especially relating to routing or duplicating), " \
+        L"Audio Router version 0.10.1.\n" \
+        L"\nIf you come across any bugs (especially relating to routing or duplicating), " \
         L"or just have an idea for a new feature, " \
         L"please send a PM to the developer on reddit: reddit.com/user/audiorouterdev/", 
         L"About", MB_ICONINFORMATION);
